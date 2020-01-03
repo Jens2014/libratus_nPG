db3e44f86504500e6e96a5689578c176a5304459
Jens2014 <jens.kossmagk@gmx.de>
2020-01-03 10:15:12 +0100

    updated

--- libratus/theme_description.php
+++ libratus/theme_description.php
@@ -1,7 +1,7 @@
 <?php
-$theme_description['name'] = 'Libratus';
-$theme_description['author'] = 'gjr - oswebcreations.com';
-$theme_description['version'] = '0.9.1';
-$theme_description['date'] = '7/8/19';
-$theme_description['desc'] = 'A Zenphoto/Zenpage theme with many new features focused on statistics (latest, ratings, popular, hits). Beta release. ';
+$theme_description['name'] = 'Libratus nPG';
+$theme_description['author'] = 'Jens Kossmagk - jekophoto.de';
+$theme_description['version'] = '0.9.3';
+$theme_description['date'] = '2020-01-03';
+$theme_description['desc'] = 'A responsive netPhotoGraphics theme with many new features focused on statistics (latest, ratings, popular, hits). Beta release. ';
 ?>
