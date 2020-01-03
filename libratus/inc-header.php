8ae1fef56b18fa18e2a1d6ac144dcd2e6694f119
Jens2014 <jens.kossmagk@gmx.de>
2020-01-03 10:52:16 +0100

    fixed GA tracking code option

--- libratus/inc-header.php
+++ libratus/inc-header.php
@@ -70,7 +70,7 @@ if (is_object($randomImage) && $randomImage->exists) {
 		(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
 		m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 		})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-		ga('create', '<?php echo getOption('zpbase_analytics'); ?>', 'auto');
+		ga('create', '<?php echo getOption('libratus_analytics'); ?>', 'auto');
 		ga('send', 'pageview');
 		<?php } else { ?>
 		var _gaq = _gaq || [];
