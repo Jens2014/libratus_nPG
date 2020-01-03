2d510173559abd438686c1380162194f533b74d5
Jens2014 <jens.kossmagk@gmx.de>
2020-01-03 20:48:30 +0100

    charset will be served by theme_head

--- libratus/slideshow.php
+++ libratus/slideshow.php
@@ -10,7 +10,6 @@ if (function_exists('printSlideShow')) {
 			<?php npgFilters::apply('theme_head'); ?>
 			<meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no">
 			<title><?php printBareGalleryTitle(); ?> <?php echo gettext("Slideshow"); ?></title>
-			<meta charset="<?php echo LOCAL_CHARSET; ?>">
 			<link rel="stylesheet" href="<?php echo $_themeroot; ?>/css/style.css" type="text/css" />
 			<link rel="stylesheet" href="<?php echo $_themeroot; ?>/css/slideshow.css" type="text/css" />
 		</head>
